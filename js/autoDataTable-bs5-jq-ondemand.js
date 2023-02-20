9cb94e4006e77c96928e354088aa86a28bb03687
Massimo Cassandro <massimo.cassandro@gmail.com>
2023-02-20 12:06:13 +0100

    1.0.3 - Fix

--- js/autoDataTable-bs5-jq-ondemand.js
+++ js/autoDataTable-bs5-jq-ondemand.js
@@ -1,9 +1,9 @@
-import {_creaDataTable} from './creaDataTable-bs5-jq-ondemand';
+import {_creaDataTableOnDemand} from './creaDataTable-bs5-jq-ondemand';
 import autoDT from './src/autoDataTable-src';
 import { jquery_loader } from '@massimo-cassandro/js-utilities';
 
 export  function _autoDataTableOnDemand( $container = '.dt-container', cdt_options = {}, jquery_url='https://code.jquery.com/jquery-3.6.3.min.js' ) {
   return jquery_loader(jquery_url, () => {
-    return autoDT( $container, cdt_options, _creaDataTable );
+    return autoDT( $container, cdt_options, _creaDataTableOnDemand );
   });
 }
