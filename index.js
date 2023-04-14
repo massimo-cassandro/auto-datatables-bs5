9ff3223fe3ad0dc1d225a67b5f8da23f9d7a486b
Massimo Cassandro <massimo.cassandro@gmail.com>
2023-04-14 16:13:25 +0200

    2.1.1 - `dt_classes` export

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 export {_autoDataTable} from './src/auto-datatable-bs5.js';
 // per eventuali personalizzazioni
-export { dt_default_options } from './src/js/datatable-defaults.js';
+export { dt_default_options, dt_classes } from './src/js/datatable-defaults.js';
